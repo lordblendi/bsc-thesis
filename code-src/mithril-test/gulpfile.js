6e8a47cf025cb9a0d9bb52d05efe4db535a8765e
Nora Szepes <lordblendi@gmail.com>
2015-10-30 10:06:04 +0000

    gulpfile: finished browserify
    reorganzied codes
    
    git-svn-id: svn://db.bme.hu/szepesnora@32 43c422d4-f008-4547-b00f-8b70f95b822d

--- code-src/mithril-test/gulpfile.js
+++ code-src/mithril-test/gulpfile.js
@@ -1,5 +1,34 @@
 var gulp = require('gulp');
 var drakov = require ('drakov');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var minifycss = require('gulp-minify-css');
+var minifyHTML = require('gulp-minify-html');
+
+gulp.task('scripts', function() {
+    gulp.src('./js/*.js')
+    .pipe(concat('index.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('html', function() {
+	var opts = {
+    conditionals: true,
+    spare:true
+  };
+  
+    gulp.src('./html/*.html')
+    .pipe(minifyHTML(opts))
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('css', function() {
+    gulp.src('./css/*.css')
+    .pipe(concat('index.css'))
+    .pipe(minifycss())
+    .pipe(gulp.dest('./dist/'))
+});
 
 gulp.task ( 'drakov-server', function () {
 
@@ -12,6 +41,11 @@ gulp.task ( 'drakov-server', function () {
 
 });
 
+gulp.task('browserify', function(){
+		gulp.start('scripts').start('html').start('css');
+	});
+
 gulp.task('default', function () {
-  return gulp.start('drakov-server');
+  //return gulp.start('drakov-server');
+  return gulp.start('browserify');
 });
